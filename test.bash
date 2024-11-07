c66c2c7bf8c10cef441be6da722ceae5d6948620
Yuki Nishijima <s23C1101EC@s.chibakoudai.jp>
2024-11-07 11:41:13 +0900

    a

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng (){
 	echo ${1}行目が違うよ
@@ -9,5 +9,14 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo OK 
 exit $res
